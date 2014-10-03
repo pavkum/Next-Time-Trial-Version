29deda7104cf15e7936090b14869ca33bd5a0cf7
pavankumarlpersonal <pavankumarlpersonal@gmail.com>
2014-10-03 23:07:34 +0530

    2.1.4-Trial

--- minify.sh
+++ minify.sh
@@ -1,3 +1,3 @@
 minify ujs/base.js ujs/utilities.js ujs/header.js ujs/techocall.js ujs/registeredContacts.js ujs/showRemainders.js ujs/showContacts.js ujs/settings.js > www/compressed/nexttalk.js
 
-minify ujs/dialer/dialer.js > www/compressed/dialer.js
+minify ujs/utilities.js ujs/dialer/dialer.js > www/compressed/dialer.js
